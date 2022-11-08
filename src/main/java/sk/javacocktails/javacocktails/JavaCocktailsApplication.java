9f022f02cbc8a6604913fda1595ef8b8d1a6d93f
StefanFabian <stefan.fabian.159@gmail.com>
2022-11-08 17:01:45 +0100

    Spring Initializr

---
+++ src/main/java/sk/javacocktails/javacocktails/JavaCocktailsApplication.java
@@ -0,0 +1,13 @@
+package sk.javacocktails.javacocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/javacocktails/javacocktails/JavaCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.javacocktails.javacocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
