bff74c46a99911f7fe0082d6b20e787bb6f756a0
StefanFabian <stefan.fabian.159@gmail.com>
2022-11-09 03:56:10 +0100

    renamed package, added dependency spring-jpa, added spring datasource

--- src/main/java/sk/javacocktails/app/JavaCocktailsApplication.java
+++ src/main/java/sk/javacocktails/app/JavaCocktailsApplication.java
@@ -1,4 +1,4 @@
-package sk.javacocktails;
+package sk.javacocktails.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/sk/javacocktails/app/ServletInitializer.java
+++ src/main/java/sk/javacocktails/app/ServletInitializer.java
@@ -1,4 +1,4 @@
-package sk.javacocktails;
+package sk.javacocktails.app;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
